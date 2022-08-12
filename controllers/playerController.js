39fd1135bae27379cc4141bd403df8e95c78c9ca
Maham Khan <mahamimranamjad@gmail.com>
2022-08-12 11:10:21 -0700

    routes

---
+++ controllers/playerController.js
@@ -0,0 +1,15 @@
+const { Game } = require("../models");
+
+const getplayers = async (req, res) => {
+ 
+};
+
+const getplayer = async (req, res) => {
+  
+};
+
+const registerPlayer = async (req, res) => {
+    
+  };
+
+module.exports = { getplayer, getplayer, registerPlayer };
