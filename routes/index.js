b019322d674b4e4a0a6944e382a8a75aaa0fe1ed
smyp1 <pinesmickey@gmail.com>
2022-08-12 13:18:52 -0400

    filled in the routes in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,9 @@
+const router = require("express").Router();
+const gameRouter = require("./gameRoutes");
+const groupRouter = require("./groupRoutes");
+const playerRouter = require("./playerRoutes");
 
+router.use("/game", gameRouter);
+router.use("/group", groupRouter);
+router.use("./player", playerRouter);
+module.exports = router;
