c1fd23a7b77423aea5e2255faff4a914c428eb8f
smyp1 <pinesmickey@gmail.com>
2022-08-12 15:38:37 -0400

    fixed typo

--- routes/playerRoutes.js
+++ routes/playerRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const controller = require("../controllers/playerController");
 
 router.get("/", controller.getplayers);
-router.get("/:player_id", controller.getplayer);
-router.post("/:player_id", controller.registerPlayer);
+router.get("/:id", controller.getplayer);
+router.post("/:id", controller.registerPlayer);
 
 module.exports = router;
