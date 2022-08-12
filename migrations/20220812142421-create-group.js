6833cfa80393bd7ea6c7b2d6287317b6df84d4dd
smyp1 <pinesmickey@gmail.com>
2022-08-12 11:02:38 -0400

    fixed tables

--- migrations/20220812142421-create-group.js
+++ migrations/20220812142421-create-group.js
@@ -1,50 +1,52 @@
-'use strict';
+"use strict";
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('groups', {
+    await queryInterface.createTable("groups", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       date: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       groupSize: {
-        type: Sequelize.INTEGER
-      },
-      gameId:{
         type: Sequelize.INTEGER,
-        onDelete:'CASCADE',
-        references:{
-          model:'game',
-          key:'id'
-        }},
-        playerId:{
-          type: Sequelize.INTEGER,
-          onDelete:'CASCADE',
-          references:{
-            model:'players',
-            key:'id'
-          }},
+      },
+      gameId: {
+        type: Sequelize.INTEGER,
+        onDelete: "CASCADE",
+        references: {
+          model: "game",
+          key: "id",
+        },
+      },
+      playerId: {
+        type: Sequelize.INTEGER,
+        onDelete: "CASCADE",
+        references: {
+          model: "players",
+          key: "id",
+        },
+      },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('groups');
-  }
+    await queryInterface.dropTable("groups");
+  },
 };
--- models/group.js
+++ models/group.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Group extends Model {
     /**
@@ -10,18 +8,22 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Group.hasMany(models.Players, { foreignKey: 'playerId', as: 'player'})
-      Group.hasOne(models.Game, { foreignKey: 'gameId',  })
+      Group.hasMany(models.Players, { foreignKey: "playerId", as: "player" });
+      Group.hasOne(models.Game, { foreignKey: "gameId" });
     }
   }
-  Group.init({
-    date: DataTypes.DATE,
-    title: DataTypes.STRING,
-    description: DataTypes.STRING,
-    groupSize: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Group',
-  });
+  Group.init(
+    {
+      date: DataTypes.DATE,
+      title: DataTypes.STRING,
+      description: DataTypes.STRING,
+      groupSize: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Group",
+      tableName: "groups",
+    }
+  );
   return Group;
 };
