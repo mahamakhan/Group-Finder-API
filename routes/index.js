4af3aabcca551dca74d2fde205964df9c6c5dd6a
smyp1 <pinesmickey@gmail.com>
2022-08-12 14:03:35 -0400

    associsations fixed in game

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const GameRouter = require("./gameRoutes");
-const GroupRouter = require("./groupRoutes");
-const PlayerRouter = require("./playerRoutes");
+// const GroupRouter = require("./groupRoutes");
+// const PlayerRouter = require("./playerRoutes");
 
 router.use("/game", GameRouter);
-router.use("/group", GroupRouter);
-router.use("/player", PlayerRouter);
+// router.use("/group", GroupRouter);
+// router.use("/player", PlayerRouter);
 
 module.exports = router;
