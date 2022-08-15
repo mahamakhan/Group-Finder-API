021864ad58a5784ea357fae135fd6f6db156ae9c
Maham Khan <mahamimranamjad@gmail.com>
2022-08-15 10:12:31 -0700

    register working

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -1,17 +1,42 @@
 const { Player } = require("../models");
+const middleware= require('../middleware')
+
 
 const getPlayers = async (req, res) => {
   let findPlayers = await Player.findAll();
   res.send(findPlayers);
 };
 
-const getPlayer = async (req, res) => {
-  let findPlayer = await Player.findByPk(req.params.players_id);
-  res.send(findPlayer);
+const login = async (req, res) => {
+  try {
+    let player = await Player.findOne({
+      where: { email: req.body.email },
+      raw: true,
+    });
+    if (
+      player &&
+      (await middleware.comaparePassword(
+        player.passcode,
+        req.body.password
+      ))
+    ) {
+      let payload = {
+        id: player.id,
+        email: player.email,
+      };
+      let token = middleware.createToken(payload);
+      return res.send({ player: payload, token });
+    }
+    res.status(401).send({ status: "Error", msg: "Unauthorized" });
+  } catch (error) {
+    throw error;
+  }
 };
 
 const registerPlayer = async (req, res) => {
-  let createPlayer = await Player.create(req.body);
+  const { email, password, username, discord } = req.body
+  let passcode = await middleware.hashPassword(password)
+  let createPlayer = await Player.create({ email, passcode, username, discord });
   res.send(createPlayer);
 };
 
@@ -29,4 +54,4 @@ const deletePlayer = async (req, res) => {
   res.send(`player ${req.params.players_id} was deleted`);
 };
 
-module.exports = { getPlayers, getPlayer, registerPlayer, updatePlayer, deletePlayer};
+module.exports = { getPlayers, login, registerPlayer, updatePlayer, deletePlayer};
