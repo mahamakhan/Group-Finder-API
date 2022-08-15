92c0f031ca8a19028ca4e25dabc13d75116668d1
smyp1 <pinesmickey@gmail.com>
2022-08-15 10:20:45 -0400

    fixed the group router

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,35 +1,42 @@
 const { Group, Game, Player } = require("../models/");
-const { Op, literal, fn, col } = require('sequelize')
 
 const getAllGroups = async (req, res) => {
-    const groups = await Group.findAll({where: {playerId: req.param.players_id},
-    include:[{model: Game, as :'games'}]})
-    res.send (groups)
- 
+  const groups = await Group.findAll();
+  res.send(groups);
 };
 
 const getGroup = async (req, res) => {
-    const group = await Group.findByPk(req.params);
-    res.send(group);
+  const group = await Group.findByPk(req.params.group_id);
+  res.send(group);
 };
 const createGroup = async (req, res) => {
-    let group = await Group.create({...req.body, 
-        gameId:req.query.game,
-        playersId: req.query.player
-    });
-    res.send(group);
+  let gameId = req.params.game_id;
+  let playerId = req.params.player_id;
+  let group = await Group.create({
+    ...req.body,
+    gameId,
+    playersId,
+  });
+  res.send(group);
 };
 
 const updateGroup = async (req, res) => {
-    let group = await Group.update({...req.body}, { where: { id: req.params.group_id }, returning: true });
-    res.send(group);
+  let group = await Group.update(
+    { ...req.body },
+    { where: { id: req.params.group_id }, returning: true }
+  );
+  res.send(group);
 };
 
 const deleteGroup = async (req, res) => {
-    let group = await Group.destroy({ where: { id: req.params.group_id } })
-    res.send({ msg: 'Commented Deleted'});
+  let group = await Group.destroy({ where: { id: req.params.group_id } });
+  res.send({ msg: "Commented Deleted" });
 };
 
-
-
-module.exports = { getAllGroups, getGroup, createGroup, updateGroup, deleteGroup};
+module.exports = {
+  getAllGroups,
+  getGroup,
+  createGroup,
+  updateGroup,
+  deleteGroup,
+};
