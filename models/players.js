239685ebf8e76571aa2c382bf0e84090fb761915
smyp1 <pinesmickey@gmail.com>
2022-08-15 10:34:37 -0400

    fixed typo

--- models/players.js
+++ models/players.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Player.hasMany(models.Group, { foreignKey: "playersId" });
+      Player.hasMany(models.Group, { foreignKey: "playerId" });
     }
   }
   Player.init(
