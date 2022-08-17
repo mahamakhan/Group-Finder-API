688c8c2f79de953c055e21cc3adb4b3824ffa42e
smyp1 <pinesmickey@gmail.com>
2022-08-17 15:07:04 -0400

    units added

--- routes/unitRoutes.js
+++ routes/unitRoutes.js
@@ -5,11 +5,8 @@ router.get("/", controller.getAllUnits);
 router.get("/:id", controller.getUnit);
 router.get("/players/:player_id", controller.findByPlayer);
 router.get("/groups/:group_id", controller.findByGroup);
-
 router.post("/", controller.createUnit);
-
 router.put("/:id", controller.updateUnit);
-
 router.delete("/:id", controller.deleteUnit);
 
 module.exports = router;
