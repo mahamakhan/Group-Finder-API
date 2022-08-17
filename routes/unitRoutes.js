76e5b2e184e00ce371fc3897d6d1ea290ace606b
smyp1 <pinesmickey@gmail.com>
2022-08-17 15:06:33 -0400

    units added

--- routes/unitRoutes.js
+++ routes/unitRoutes.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
 const controller = require("../controllers/unitController");
 
-router.get("/", controller.getAllPlayerGroups);
-router.get("/:id", controller.getPlayerGroup);
+router.get("/", controller.getAllUnits);
+router.get("/:id", controller.getUnit);
 router.get("/players/:player_id", controller.findByPlayer);
 router.get("/groups/:group_id", controller.findByGroup);
 
-router.post("/", controller.createPlayerGroup);
+router.post("/", controller.createUnit);
 
-router.put("/:id", controller.updatePlayerGroup);
+router.put("/:id", controller.updateUnit);
 
-router.delete("/:id", controller.deletePlayerGroup);
+router.delete("/:id", controller.deleteUnit);
 
 module.exports = router;
